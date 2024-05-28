82c330b5b67f33de51a136896d5d93a13b3ae86a
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-05-29 00:27:06 +0500

    added selected options

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,29 +1,28 @@
 <template>
-  <div>
-    <div class="flex max-md:flex-col items-center max-md:text-center">
-      <div class="">
-        <p
-          class="text-sm uppercase tracking-widest font-medium text-[#3D3D3D] max-md:font-bold max-md:text-xl max-md:underline underline-offset-4"
-        >
-          Welcome to GreenShop
-        </p>
-        <h1 class="md:text-7xl max-md:text-xl font-bold text-gray-900 my-4 max-w-[500px]">
-          Let’s Make a Better <span class="text-[#46A358]">Planet</span>
-        </h1>
-        <p class="text-[#727272] max-md:text-xs mb-4 max-w-[620px]">
-          We are an online plant shop offering a wide range of cheap and trendy
-          plants. Use our plants to create an unique Urban Jungle. Order your
-          favorite plants!
-        </p>
-        <button
-          class="bg-green-500 text-white md:px-6 px-2 md:py-3 py-2 max-md:text-xs rounded-lg hover:bg-green-600 transition duration-300"
-        >
-          SHOP NOW
-        </button>
-      </div>
-      <img src="/imgs/MainBanner.png" alt="Main Banner" class="w-1/2 o" />
-    </div>
-  </div>
+	<div class="mt-10 lg:mt-0">
+		<div class="flex items-center max-md:flex-col max-md:text-center">
+			<div class="">
+				<p
+					class="text-sm uppercase tracking-widest font-medium text-[#3D3D3D] max-md:font-bold max-md:text-xl max-md:underline underline-offset-4">
+					Welcome to GreenShop
+				</p>
+				<h1
+					class="md:text-7xl max-md:text-xl font-bold text-gray-900 my-4 max-w-[500px]">
+					Let’s Make a Better <span class="text-[#46A358]">Planet</span>
+				</h1>
+				<p class="text-[#727272] max-md:text-xs mb-4 max-w-[620px]">
+					We are an online plant shop offering a wide range of cheap and trendy
+					plants. Use our plants to create an unique Urban Jungle. Order your
+					favorite plants!
+				</p>
+				<button
+					class="px-2 py-2 text-white transition duration-300 bg-green-500 rounded-lg md:px-6 md:py-3 max-md:text-xs hover:bg-green-600">
+					SHOP NOW
+				</button>
+			</div>
+			<img src="/imgs/MainBanner.png" alt="Main Banner" class="w-1/2 o" />
+		</div>
+	</div>
 </template>
 
 <script>
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,60 +1,57 @@
 <script>
 export default {
-  props: {
-    product: {
-      type: Object,
-    },
-  },
-  methods: {
-    addToCart() {
-      this.$store.dispatch("addToCart");
-    },
-  },
+	props: {
+		product: {
+			type: Object,
+		},
+	},
+	methods: {
+		addToCart() {
+			this.$store.dispatch('addToCart');
+		},
+	},
 };
 </script>
 <template>
-  <div>
-    <div class="">
-      <div class="group">
-        <div class="relative mb-10">
-          <span
-            v-if="product.sale == true"
-            class="bg-[#46A358] text-white md:text-base font-medium absolute top-4 py-1 px-2 -left-5"
-            >13% OFF</span
-          >
-          <img
-            :src="product.img"
-            alt=""
-            class="w-full border-t-2 border-white md:hover:border-green-300 bg-white"
-          />
-        
-          <div
-            class="gap-x-7 absolute flex md:hidden md:left-16 md:group-hover:flex"
-          >
-            <img
-              class="max-md:w-2/12"
-              @click="addToCart"
-              src="/icons/cart.svg"
-              alt=""
-            />
-            <img class="max-md:w-2/12" src="/icons/heart.svg" alt="" />
-            <img class="max-md:w-2/12" src="/icons/search.svg" alt="" />
-          </div>
-        </div>
-        <h2 class="md:text-base text-sm">{{ product.title }}</h2>
-        <div class="flex items-center max-md:gap-2">
-          <p class="md:text-lg text-sm font-bold text-[#46A358]">
-            {{ product.price }}
-          </p>
-          <span
-            v-if="product.sale == true"
-            class="text-[#A5A5A5] md:text-lg text-sm font-normal md:ml-5 line-through"
-            >$229.00</span
-          >
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="cursor-pointer">
+		<div class="">
+			<div class="group">
+				<div class="relative mb-10">
+					<span
+						v-if="product.sale == true"
+						class="bg-[#46A358] text-white md:text-base font-medium absolute top-4 py-1 px-2 -left-5"
+						>13% OFF</span
+					>
+					<img
+						:src="product.img"
+						alt=""
+						class="w-full bg-white border-t-2 border-white md:hover:border-green-300" />
+
+					<div
+						class="absolute flex gap-x-7 md:hidden md:left-16 md:group-hover:flex">
+						<img
+							class="max-md:w-2/12"
+							@click="addToCart"
+							src="/icons/cart.svg"
+							alt="" />
+						<img class="max-md:w-2/12" src="/icons/heart.svg" alt="" />
+						<img class="max-md:w-2/12" src="/icons/search.svg" alt="" />
+					</div>
+				</div>
+				<h2 class="text-sm md:text-base">{{ product.title }}</h2>
+				<div class="flex items-center max-md:gap-2">
+					<p class="md:text-lg text-sm font-bold text-[#46A358]">
+						{{ product.price }}
+					</p>
+					<span
+						v-if="product.sale == true"
+						class="text-[#A5A5A5] md:text-lg text-sm font-normal md:ml-5 line-through"
+						>$229.00</span
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
